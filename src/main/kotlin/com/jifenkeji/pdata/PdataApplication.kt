a06134963e64e04f44c0632769da1340fb7ff092
陈世阳 <chenshiyang@it168.com>
2019-05-24 11:04:01 +0800

    去掉无用的代码

--- src/main/kotlin/com/jifenkeji/pdata/PdataApplication.kt
+++ src/main/kotlin/com/jifenkeji/pdata/PdataApplication.kt
@@ -3,14 +3,13 @@ package com.jifenkeji.pdata
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @MapperScan(basePackages = ["com.jifenkeji.pdata.mapper"])
 class PdataApplication
 
 fun main(args: Array<String>) {
-    println("helloworld")
-    println("xxxx")
     runApplication<PdataApplication>(*args)
-
 }
