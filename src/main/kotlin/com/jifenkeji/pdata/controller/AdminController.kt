0bd23307f3f0bf6ec76ae1ccd116339db7067385
陈世阳 <chenshiyang@it168.com>
2019-06-27 14:08:20 +0800

    修改增加管理员

--- src/main/kotlin/com/jifenkeji/pdata/controller/AdminController.kt
+++ src/main/kotlin/com/jifenkeji/pdata/controller/AdminController.kt
@@ -1,11 +1,13 @@
 package com.jifenkeji.pdata.controller
 
+import com.jifenkeji.pdata.entity.Admin
 import com.jifenkeji.pdata.service.AdminService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -20,5 +22,29 @@ class AdminController {
         val admins = adminService.findAll()
         model.set("admins", admins)
         return "admin/list"
+    }
+
+    @GetMapping("add")
+    fun add(model: Model): Any? {
+        model.set("admin", Admin())
+        return "admin/add"
+    }
+
+    @PostMapping("add_save")
+    fun addSave(model: Model, admin: Admin?): Any? {
+        //
+        admin ?: return "redirect:add"
+
+        adminService.add(admin)
+
+        return "redirect:list"
+    }
+    @GetMapping("del")
+    fun del(id: Int?): Any? {
+        id ?: return "redirect:/admin/list"
+
+        adminService.removeByPrimaryKey(id)
+
+        return "redirect:/admin/list"
     }
 }
